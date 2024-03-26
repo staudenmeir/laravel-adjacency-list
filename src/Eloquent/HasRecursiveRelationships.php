09030e4e0d0bbddece587be71aa4f119b09f1e53
Seweryn Czabanowski <seweryn_cz@outlook.com>
2024-03-26 19:55:48 +0100

    Add new methods to check

--- src/Eloquent/HasRecursiveRelationships.php
+++ src/Eloquent/HasRecursiveRelationships.php
@@ -2,6 +2,7 @@
 
 namespace Staudenmeir\LaravelAdjacencyList\Eloquent;
 
+use Illuminate\Database\Eloquent\Model;
 use Staudenmeir\LaravelAdjacencyList\Eloquent\Traits\HasAdjacencyList;
 use Staudenmeir\LaravelCte\Eloquent\QueriesExpressions;
 
@@ -9,4 +10,19 @@ trait HasRecursiveRelationships
 {
     use HasAdjacencyList;
     use QueriesExpressions;
+
+    public function isParentOf(Model $user)
+    {
+        return $this->children->contains($user);
+    }
+
+    public function isChildOf(Model $user)
+    {
+        return $this->parent->is($user);
+    }
+
+    public function depthRelatedTo(Model $user)
+    {
+        return $this->ancestors->contains($user) ? $this->ancestors->indexOf($user) + 1 : null;
+    }
 }
