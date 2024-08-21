fd109daf2148105055c1d52179219187fc7598db
Sander Muller <github@scode.nl>
2024-08-22 00:22:44 +0200

    Improve relation generics (#252)

--- src/Eloquent/Builder.php
+++ src/Eloquent/Builder.php
@@ -5,6 +5,11 @@ namespace Staudenmeir\LaravelAdjacencyList\Eloquent;
 use Illuminate\Database\Eloquent\Builder as Base;
 use Staudenmeir\LaravelAdjacencyList\Eloquent\Traits\BuildsAdjacencyListQueries;
 
+/**
+ * @template TModel of \Illuminate\Database\Eloquent\Model
+ *
+ * @extends Base<TModel>
+ */
 class Builder extends Base
 {
     use BuildsAdjacencyListQueries;
