f8b6ce02d1e94517f74b92340792f3eeefe111dc
Tyler <tylernathanreed@gmail.com>
2024-09-05 14:37:42 -0500

    #259: Fixed typo

--- src/Eloquent/Collection.php
+++ src/Eloquent/Collection.php
@@ -65,7 +65,7 @@ class Collection extends Base
 
         if (! method_exists($instance, 'getPathName') || ! method_exists($instance, 'getPathSeparator')) {
             throw new RuntimeException(sprintf(
-                'Model [%s] is not have recusive relations.',
+                'Model [%s] does not have recusive relations.',
                 $instance::class,
             ));
         }
