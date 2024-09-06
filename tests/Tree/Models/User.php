386c15661de15c4f31a7a39c2802a665ceaa4a72
Tyler Reed <tylernathanreed@gmail.com>
2024-09-06 09:20:29 -0500

    #259: Added missing include

--- tests/Tree/Models/User.php
+++ tests/Tree/Models/User.php
@@ -2,6 +2,7 @@
 
 namespace Staudenmeir\LaravelAdjacencyList\Tests\Tree\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Query\Expression;
